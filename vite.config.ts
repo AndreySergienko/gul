0da36879d2bfd7b7e77a8b1545a04db52e59915d
Andrey Sergineko <getmessage@gmail.com>
2024-07-09 11:06:15 +0500

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import * as pathNode from 'path'
+import vue from '@vitejs/plugin-vue'
+
+const SOURCE = './src'
+
+/** Создать путь, используя корневую папку */
+const createAliasPath = (path?: string) => {
+  const correctPath = path ? `${SOURCE}/${path}` : SOURCE
+  return pathNode.resolve(__dirname, correctPath)
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': createAliasPath(),
+      '@type': createAliasPath('types'),
+      '@controller': createAliasPath('controllers'),
+      '@component': createAliasPath('components'),
+      '@widget': createAliasPath('widget'),
+      '@composable': createAliasPath('composables'),
+    },
+  },
+})
